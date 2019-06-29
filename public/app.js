cf6e205eba98634ec07d6f3a83465aae3d7e8f98
bradleyjd19 <bradleyjd19@gmail.com>
2019-06-29 18:30:08 -0500

    added models and handlebars layout

---
+++ public/app.js
@@ -0,0 +1,46 @@
+$.getJSON("/articles", (data) {
+  for (let i = 0; i < data.length; i++) {
+    $("#").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "</p>");
+  }
+});
+
+$(document).on("click", "p", () => {
+  $("#").empty();
+  let thisId = $(this).attr("data-id");
+
+  $.ajax({
+    method: "GET",
+    url: "/articles" + thisId
+  })
+    .then((data) => {
+      console.log(data);
+      $("#").append("<h2>" + data.title + "</h2>");
+      $("#").append("<input id='titleinput' name='title' >");
+      $("#").append("<textarea id='bodyinput' name='body'></textarea>");
+      $("#").append("<button data-id='" + data._id + "' id='savenote'>Save Note</button>");
+
+      if (data.note) {
+        $("#").val(data.note.title);
+        $("#").val(data.note.body);
+      }
+    });
+});
+
+$(document).on("click", "#", () => {
+  let thisId = $(this).attr("data-id");
+
+  $.ajax({
+    method: "POST",
+    url: "/articles" + thisId,
+    data: {
+      title: $("#").val(),
+      body: $("#").val();
+    }
+  })
+    .then((data) => {
+      console.log(data);
+      $("#").empty();
+    });
+  $("#").val("");
+  $("#").val("");
+})
--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const logger = require("morgan");
 const axios = require("axios");
 const cheerio = require("cheerio");
 
-// const db = require("./models");
+const db = require("./models");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -20,6 +20,15 @@ mongoose.connect("mongodb://localhost/broArticles", { useNewUrlParser: true });
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
+app.get("/", (req, res) => {
+  db.Article.find({ saved: false }, (error, data) => {
+    let hbsObject = {
+      article: data
+    };
+    res.render("index", hbsObject);
+  });
+});
+
 app.get("/scrape", (req, res) => {
   axios.get("https://www.brobible.com")
     .then((response) => {
@@ -32,17 +41,51 @@ app.get("/scrape", (req, res) => {
           .find("a").attr("href");
         result.photo = $(element)
           .find("div").find("a").attr("href");
-          console.log(result);
-        // db.Article.create(result)
-        //   .then((dbArticle) => {
-        //     console.log(dbArticle);
-        //   })
-          // .catch((err) => {
-          //   console.log(err);
-          // });
+        console.log(result);
+        db.Article.create(result)
+          .then((dbArticle) => {
+            console.log(dbArticle);
+          })
+          .catch((err) => {
+            console.log(err);
+          });
       });
     });
   res.send("Scrape Complete");
+});
+
+app.get("/articles", (req, res) => {
+  db.Article.find({})
+    .then((dbArticle) => {
+      res.json(dbArticle);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
+app.get("/articles/:id", (req, res) => {
+  db.Article.findOne({ _id: req.params.id })
+    .populate("note")
+    .then((dbArticle) => {
+      res.json(dbArticle)
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
+app.post("/articles/:id", (req, res) => {
+  db.Note.create(req.body)
+    .then((dbNote) => {
+      return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
+    })
+    .then((dbArticle) => {
+      res.json(dbArticle);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
 });
 
 app.listen(PORT, () => {
